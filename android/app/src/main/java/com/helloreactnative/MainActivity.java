713b155f800bd05b80200995ecf375ab99c8e4e4
Frewen.Wong <frewen1225@gmail.com>
2020-08-05 17:56:46 +0800

    [REMOVE] 移除权限检查的相关逻辑

--- android/app/src/main/java/com/helloreactnative/MainActivity.java
+++ android/app/src/main/java/com/helloreactnative/MainActivity.java
@@ -1,27 +1,24 @@
 package com.helloreactnative;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.modules.core.PermissionAwareActivity;
-import com.facebook.react.modules.core.PermissionListener;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MainActivity extends ReactActivity {
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-public class MainActivity extends ReactActivity implements PermissionAwareActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initPermission();
+        Log.d(TAG, "FMsg:onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "FMsg:onResume() called");
     }
 
     /**
@@ -33,72 +30,21 @@ public class MainActivity extends ReactActivity implements PermissionAwareActivi
         return "HelloReactNative";
     }
 
-    private void initPermission() {
-        List<String> permissionList = new ArrayList<>();
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            permissionList.add(Manifest.permission.READ_PHONE_STATE);
-        }
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
-            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
-        }
-
-        if (!permissionList.isEmpty()) {
-            ActivityCompat.requestPermissions(MainActivity.this, permissionList.toArray(new String[permissionList.size()]), 1);
-        } else {
-        }
-    }
-
-    /**
-     * 当有多个权限需要申请的时候
-     * 这里以打电话和SD卡读写权限为例
-     */
-    private void requestPermissions() {
-
-        List<String> permissionList = new ArrayList<>();
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            permissionList.add(Manifest.permission.CALL_PHONE);
-        }
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
-
-        if (!permissionList.isEmpty()) {  //申请的集合不为空时，表示有需要申请的权限
-            ActivityCompat.requestPermissions(this, permissionList.toArray(new String[permissionList.size()]), 1);
-        } else { //所有的权限都已经授权过了
-
-        }
-    }
-
-    /**
-     * 权限申请返回结果
-     *
-     * @param requestCode  请求码
-     * @param permissions  权限数组
-     * @param grantResults 申请结果数组，里面都是int类型的数
-     */
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { //同意权限申请
-
-                } else { //拒绝权限申请
-                    Toast.makeText(this, "权限被拒绝了", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-                break;
-        }
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "FMsg:onPause() called");
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "FMsg:onStop() called");
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "FMsg:onDestroy() called");
+    }
 }
