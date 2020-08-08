453958e386260b8c4966bc1a034b605698656f9f
Frewen.Wong <frewen1225@gmail.com>
2020-08-08 21:34:34 +0800

    [ADD] add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,23 @@ module.exports = {
     root: true,
     extends: '@react-native-community',
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', "prettier"],
+    plugins: ['@typescript-eslint', 'prettier'],
     rules: {
-        "prettier/prettier": "error",
-        "no-undef": 0,
-        "no-useless-constructor": 0
+        'prettier/prettier': 'error',
+        'no-undef': 0,
+        'no-useless-constructor': 0,
     },
     extends: [
         // 'standard', //使用standard做代码规范
-        "prettier",
+        'prettier',
         // "react-app",
-        "plugin:prettier/recommended"
+        'plugin:prettier/recommended',
     ],
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "modules": true
-        }
+    'parserOptions': {
+        'ecmaVersion': 6,
+        'sourceType': 'module',
+        'ecmaFeatures': {
+            'modules': true,
+        },
     },
 };
--- toolkits/metro/multi_bundle/getModulelId.js
+++ toolkits/metro/multi_bundle/getModulelId.js
@@ -18,74 +18,73 @@ let bizMappingPath;
  * isBiz:是否是业务包
  * */
 function getModuleIdByIndex(projectRootPath, path, entry, isBiz) {
-  // 获取根据ID来进行分包的ID配置Json
-  const moduleIdConfig = require('./moduleIdConfig.json');
-  if (curPlatformModuleId == -100) {
-    curPlatformModuleId = -1; //基础包的Module都是从0开始的
-  }
+    // 获取根据ID来进行分包的ID配置Json
+    const moduleIdConfig = require('./moduleIdConfig.json');
+    if (curPlatformModuleId == -100) {
+        curPlatformModuleId = -1; //基础包的Module都是从0开始的
+    }
 
-  // 根据ID进行多Bundle分包的映射地址
-  if (baseMappingPath == null) {
-    baseMappingPath = __dirname + pathSep + 'platformIDMap.json';
-  }
-  if (baseModuleIdMap.length == 0) {
-    if (fs.existsSync(baseMappingPath)) {
-      baseModuleIdMap = require(baseMappingPath);
-      if (baseModuleIdMap.length != 0) {
-        // 如果映射文件里面的映射数据不为空，那么按照已经存在的ID开始计算后续ID
-        curPlatformModuleId = baseModuleIdMap[baseModuleIdMap.length - 1].id;
-      }
+    // 根据ID进行多Bundle分包的映射地址
+    if (baseMappingPath == null) {
+        baseMappingPath = __dirname + pathSep + 'platformIDMap.json';
     }
-  }
+    if (baseModuleIdMap.length == 0) {
+        if (fs.existsSync(baseMappingPath)) {
+            baseModuleIdMap = require(baseMappingPath);
+            if (baseModuleIdMap.length != 0) {
+                // 如果映射文件里面的映射数据不为空，那么按照已经存在的ID开始计算后续ID
+                curPlatformModuleId = baseModuleIdMap[baseModuleIdMap.length - 1].id;
+            }
+        }
+    }
 
-  // 业务模块的的映射文件计算
-  if (isBiz) {
-    if (bizMappingPath == null) {
-      bizMappingPath =
-        __dirname + pathSep + entry.replace('.js', '') + 'IDMap.json';
+    // 业务模块的的映射文件计算
+    if (isBiz) {
+        if (bizMappingPath == null) {
+            bizMappingPath = __dirname + pathSep + entry.replace('.js', '') + 'IDMap.json';
+        }
+        if (bizModuleIdMap.length == 0) {
+            if (fs.existsSync(bizMappingPath)) {
+                bizModuleIdMap = require(bizMappingPath);
+                // 设置当前的ModuleID为映射表的最后一个ID
+                curBizModuleId = bizModuleIdMap[bizModuleIdMap.length - 1].id;
+            } else if (curBizModuleId == -100) {
+                //根据业务包moduleID的配置取初始moduleId。如果从来没有生成果映射文件
+                // 则使用我们配置的开始ID
+                curBizModuleId = moduleIdConfig[entry] - 1;
+            }
+        }
     }
-    if (bizModuleIdMap.length == 0) {
-      if (fs.existsSync(bizMappingPath)) {
-        bizModuleIdMap = require(bizMappingPath);
-        // 设置当前的ModuleID为映射表的最后一个ID
-        curBizModuleId = bizModuleIdMap[bizModuleIdMap.length - 1].id;
-      } else if (curBizModuleId == -100) {
-        //根据业务包moduleID的配置取初始moduleId。如果从来没有生成果映射文件
-        // 则使用我们配置的开始ID
-        curBizModuleId = moduleIdConfig[entry] - 1;
-      }
+    // 相对路径
+    let pathRelative = null;
+    if (path.indexOf(projectRootPath) == 0) {
+        pathRelative = path.substr(projectRootPath.length + 1);
     }
-  }
-  // 相对路径
-  let pathRelative = null;
-  if (path.indexOf(projectRootPath) == 0) {
-    pathRelative = path.substr(projectRootPath.length + 1);
-  }
-  const findPlatformItem = baseModuleIdMap.find((value) => {
-    return value.path == pathRelative;
-  });
-  const findBizItem = bizModuleIdMap.find((value) => {
-    return value.path == pathRelative;
-  });
-  if (findPlatformItem) {
-    return findPlatformItem.id;
-  } else if (findBizItem) {
-    return findBizItem.id;
-  } else {
-    if (!isBiz) {
-      //创建平台基础包的映射文件
-      curPlatformModuleId = ++curPlatformModuleId;
-      baseModuleIdMap.push({id: curPlatformModuleId, path: pathRelative});
-      fs.writeFileSync(baseMappingPath, JSON.stringify(baseModuleIdMap));
-      return curPlatformModuleId;
+    const findPlatformItem = baseModuleIdMap.find((value) => {
+        return value.path == pathRelative;
+    });
+    const findBizItem = bizModuleIdMap.find((value) => {
+        return value.path == pathRelative;
+    });
+    if (findPlatformItem) {
+        return findPlatformItem.id;
+    } else if (findBizItem) {
+        return findBizItem.id;
     } else {
-      //创建业务包业务包
-      curBizModuleId = ++curBizModuleId;
-      bizModuleIdMap.push({id: curBizModuleId, path: pathRelative});
-      fs.writeFileSync(bizMappingPath, JSON.stringify(bizModuleIdMap));
-      return bizModuleIdMap;
+        if (!isBiz) {
+            //创建平台基础包的映射文件
+            curPlatformModuleId = ++curPlatformModuleId;
+            baseModuleIdMap.push({id: curPlatformModuleId, path: pathRelative});
+            fs.writeFileSync(baseMappingPath, JSON.stringify(baseModuleIdMap));
+            return curPlatformModuleId;
+        } else {
+            //创建业务包业务包
+            curBizModuleId = ++curBizModuleId;
+            bizModuleIdMap.push({id: curBizModuleId, path: pathRelative});
+            fs.writeFileSync(bizMappingPath, JSON.stringify(bizModuleIdMap));
+            return bizModuleIdMap;
+        }
     }
-  }
 }
 
 /**
@@ -93,29 +92,19 @@ function getModuleIdByIndex(projectRootPath, path, entry, isBiz) {
  * 优点是简单且确保唯一，缺点是无法使用RamBundle打包方式
  **/
 function getModuleIdByName(projectRootPath, path) {
-  console.log('getModuleIdByName', `projectRootPath ： ${projectRootPath}`);
-  console.log('getModuleIdByName', `path ： ${path}`);
-  let name = '';
-  if (
-    path.indexOf(
-      'node_modules' +
-        pathSep +
-        'react-native' +
-        pathSep +
-        'Libraries' +
-        pathSep,
-    ) > 0
-  ) {
-    name = path.substr(path.lastIndexOf(pathSep) + 1);
-  } else if (path.indexOf(projectRootPath) == 0) {
-    name = path.substr(projectRootPath.length + 1);
-  }
-  name = name.replace('.js', '');
-  name = name.replace('.png', '');
-  let regExp =
-    pathSep == '\\' ? new RegExp('\\\\', 'gm') : new RegExp(pathSep, 'gm');
-  name = name.replace(regExp, '_'); //把path中的/换成下划线
-  return name;
+    console.log('getModuleIdByName', `projectRootPath ： ${projectRootPath}`);
+    console.log('getModuleIdByName', `path ： ${path}`);
+    let name = '';
+    if (path.indexOf('node_modules' + pathSep + 'react-native' + pathSep + 'Libraries' + pathSep) > 0) {
+        name = path.substr(path.lastIndexOf(pathSep) + 1);
+    } else if (path.indexOf(projectRootPath) == 0) {
+        name = path.substr(projectRootPath.length + 1);
+    }
+    name = name.replace('.js', '');
+    name = name.replace('.png', '');
+    let regExp = pathSep == '\\' ? new RegExp('\\\\', 'gm') : new RegExp(pathSep, 'gm');
+    name = name.replace(regExp, '_'); //把path中的/换成下划线
+    return name;
 }
 
 /**
@@ -127,10 +116,10 @@ function getModuleIdByName(projectRootPath, path) {
  * @returns {string|number}
  */
 function getModuleId(projectRootPath, path, entry, isBiz) {
-  if (useIndex) {
-    return getModuleIdByIndex(projectRootPath, path, entry, isBiz);
-  }
-  return getModuleIdByName(projectRootPath, path);
+    if (useIndex) {
+        return getModuleIdByIndex(projectRootPath, path, entry, isBiz);
+    }
+    return getModuleIdByName(projectRootPath, path);
 }
 
 /**
